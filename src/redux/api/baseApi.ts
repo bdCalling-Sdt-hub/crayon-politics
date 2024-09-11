b773008913dc25006a3202535c2f3a810c4c09cb
Nadir Hossain <nadirhossain336@gmail.com>
2024-09-11 12:12:37 +0600

    :bug: fix a bug

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://api.crayonpolitics.org/api/v1"
+        baseUrl: "https://api.crayonpolitics.org/api/v1"
     }),
     endpoints: () => ({})
 });
 
-export const imageUrl = "http://api.crayonpolitics.org";
+export const imageUrl = "https://api.crayonpolitics.org";
