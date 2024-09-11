ccfaa2a220e5ff3b4ec106bc73d750995d32491f
Nadir Hossain <nadirhossain336@gmail.com>
2024-09-11 11:29:35 +0600

    :package: change base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://142.93.201.200:5000/api/v1"
+        baseUrl: "http://api.crayonpolitics.org/api/v1"
     }),
     endpoints: () => ({})
 });
 
-export const imageUrl = "http://142.93.201.200:5000";
+export const imageUrl = "http://api.crayonpolitics.org";
