08db8c262b8ef6f27ffdfbae6144d9cbd27debfc
Nadir Hossain <nadirhossain336@gmail.com>
2024-09-11 11:03:33 +0600

    :package: change base api

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://143.244.131.216:5000/api/v1"
+        baseUrl: "http://142.93.201.200:5000/api/v1"
     }),
     endpoints: () => ({})
 });
 
-export const imageUrl = "http://143.244.131.216:5000";
+export const imageUrl = "http://142.93.201.200:5000";
